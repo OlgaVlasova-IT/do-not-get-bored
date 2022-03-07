71852f78d20b205f72338072336ad5274b035891
Olga Vlasova <73309221+OlgaVlasova-IT@users.noreply.github.com>
2022-03-07 10:26:01 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./App.css";
+import { useState, useEffect } from "react";
+
+function App() {
+  const [activity, setActivity] = useState("");
+  async function fetchActivity() {
+    const response = await fetch("http://www.boredapi.com/api/activity/");
+    const data = await response.json();
+    setActivity(data.activity);
+  }
+
+  useEffect(() => {
+    fetchActivity();
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>{activity}</p>
+        <button
+          onClick={() => {
+            fetchActivity();
+          }}
+          className="btn"
+        >
+          Next activity
+        </button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
